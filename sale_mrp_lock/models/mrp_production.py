b984d6c123e21a9c689b2774ffe3a9aa9dd86fed
g <ghoostinger@mail.com>
2022-01-21 18:44:08 +0000

    [FIX] mrp_production auth_editing

--- sale_mrp_lock/models/mrp_production.py
+++ sale_mrp_lock/models/mrp_production.py
@@ -5,7 +5,11 @@ from odoo.exceptions import UserError
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
+    # .with_context(auth_editing=True)
     def write(self, vals):
-        for rec in self.sudo().filtered(lambda i: i.sale_id and i.sale_id.state == 'done'):
-            raise UserError('L\'ordre de fabrication est bloquée par la commande ' + rec.sale_id.name)
+        if self._context.get('auth_editing') and self._context.get('auth_editing') == True:
+            pass
+        else:
+            for rec in self.sudo().filtered(lambda i: i.sale_id and i.sale_id.state == 'done'):
+                raise UserError('L\'ordre de fabrication est bloquée par la commande ' + rec.sale_id.name)
         return super().write(vals)
