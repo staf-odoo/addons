e45fed3f80aacbee720d532b7ccab6d1cf98a5f6
ghoostinger <74766557+ghoostinger@users.noreply.github.com>
2023-07-14 21:53:18 +0000

    Update sale.py

--- sale_uom_dimension/models/sale.py
+++ sale_uom_dimension/models/sale.py
@@ -9,9 +9,9 @@ class SaleOrderLine(models.Model):
 
     dimension_ids = fields.One2many('sale.order.line.dimension', 'line_id', string='Dimensions', copy=True)
 
-    @api.depends(_qty_field)
-    def _get_product_dimension_qty(self):
-        super()._get_product_dimension_qty()
+    # @api.depends(_qty_field)
+    # def _get_product_dimension_qty(self):
+    #     super()._get_product_dimension_qty()
 
     @api.onchange(_uom_field)
     def onchange_product_uom_set_dimensions(self):
