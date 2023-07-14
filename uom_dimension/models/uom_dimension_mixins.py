ae101906da0f3acc835700ffa1641794a935a713
ghoostinger <74766557+ghoostinger@users.noreply.github.com>
2023-07-14 22:49:27 +0000

    Update uom_dimension_mixins.py

--- uom_dimension/models/uom_dimension_mixins.py
+++ uom_dimension/models/uom_dimension_mixins.py
@@ -27,6 +27,11 @@ class UomLine(models.AbstractModel):
     #         if self.dimension_ids:
     #         rec[rec._qty_field] = rec[rec._uom_field].eval_values(dict([(d.dimension_id.id, d.quantity) for d in self.dimension_ids]),
     #                                            self.product_dimension_qty)
+    @api.onchange('product_dimension_qty', 'dimension_ids')
+    def onchange_dimension_ids(self):
+        if self.dimension_ids:
+            self[self._qty_field] = self[self._uom_field].eval_values(dict([(d.dimension_id.id, d.quantity) for d in self.dimension_ids]),
+                                               self.product_dimension_qty)
 
     def _get_product_dimension_qty(self):
         for rec in self:
