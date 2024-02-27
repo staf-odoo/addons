25705e0f60283439a5ec4cfc7e68de2be7b2eb96
DOUL <ghoostinger@github.com>
2024-02-27 19:47:45 +0000

    [UPD] sale_invoice_lock add group account_supp

--- sale_invoice_lock/models/account_invoice.py
+++ sale_invoice_lock/models/account_invoice.py
@@ -7,6 +7,8 @@ class AccountInvoice(models.Model):
 
     def write(self, vals):
         for rec in self.filtered(lambda i: any(s.state == 'done' for s in i.invoice_line_ids.mapped('sale_line_ids.order_id'))):
-            if not self.env.user.has_group('base.group_erp_manager') and not self.env.user.has_group('base_exception.group_exception_rule_manager'):
+            if (not self.env.user.has_group('base.group_erp_manager')
+                    and not self.env.user.has_group('base_exception.group_exception_rule_manager')
+                    and not self.env.user.has_group('staf_meta.group_account_supp')):
                 raise UserError('La commande relative est bloquée')
         return super().write(vals)
